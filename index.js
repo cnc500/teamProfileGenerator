3619d91afedbb8606ab8e93f376821222b668fc9
Christian Carnevale <cncarnevale@gmail.com>
2022-04-17 16:16:39 -0400

    converted array into function within employee.js, though with error

--- index.js
+++ index.js
@@ -1,44 +1,39 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const employee = require("./lib/employee")
+const manager = require("./lib/manager")
 
 // need to put questionNextTeamMember in function getRole() in employee.js
 // need to create getName(), getId() and getEmail() in employee.js
 
 
-const questionNextTeamMember = [
-    {
-        type : "list",
-        name : "choice",
-        message : "Which type of team member do you want to add?",
-        choices : ["Engineer","Intern","I am finished adding team members."]
-    },
+
 
 function init() {
     inquirer.prompt(employee.questionsManager);
         getRole();
 //    inquirer.prompt(questionNextTeamMember);
-    if (choices==="Engineer"){   
-        getName(); 
-        getId(); 
-        getEmail();
-        getGithub();
-    } else if (choices==="Intern") {
-        getName(); 
-        getId(); 
-        getEmail();
-        getSchool();
-    } else {
-        // message "You have assembles an awesome team!"
-    }
-    }
+    // if (choices==="Engineer"){   
+    //     getName(); 
+    //     getId(); 
+    //     getEmail();
+    //     getGithub();
+    // } else if (choices==="Intern") {
+    //     getName(); 
+    //     getId(); 
+    //     getEmail();
+    //     getSchool();
+    // } else {
+    //     // message "You have assembles an awesome team!"
+    // }
+    // }
 
 
-        inquirer.prompt(employee.questionsEngineer);
+    //     inquirer.prompt(employee.questionsEngineer);
 
-    }
-    .then(data=>{
-        console.log(data);
-    }
+    // }
+    // .then(data=>{
+    //     console.log(data);
+    // }
 }
 
