0ccadc36fffbc7504d6e7befde1ca53cb480bf05
Christian Carnevale <cncarnevale@gmail.com>
2022-04-19 08:13:49 -0400

    functions added within class Manager and Employee, index with working questionManager

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const employee = require("./lib/employee")
-const manager = require("./lib/manager")
+const Employee = require("./lib/employee")
+const Manager = require("./lib/manager")
 
 // need to put questionNextTeamMember in function getRole() in employee.js
 // need to create getName(), getId() and getEmail() in employee.js
 
-
+const questionsManager = [
+    {
+        type : "input",
+        name : "name",
+        message : "What is the name of your team's manager?",
+    },
+    {
+        type : "input",
+        name : "iD",
+        message : "What is your team manager's ID number (positve number only)?",
+    },
+    {
+        type : "input",
+        name : "email",
+        message : "What is your team manager's email address?",
+    },
+    {
+        type : "input",
+        name : "officeNumber",
+        message : "What is your team manager's office number?",
+    },
+];
 
 
 function init() {
-    inquirer.prompt(employee.questionsManager);
-        getRole();
+    inquirer.prompt(questionsManager)
+        .then(data=>{
+        console.log(data);
+    })    
+}
+
+// Calls function init to initialize app
+init();
 //    inquirer.prompt(questionNextTeamMember);
     // if (choices==="Engineer"){   
     //     getName(); 
@@ -35,5 +62,5 @@ function init() {
     // .then(data=>{
     //     console.log(data);
     // }
-}
+
 
